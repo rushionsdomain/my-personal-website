262bb6cd37aa69c1bebcc73b824f9f70f41248e9
Rushion <rushionchegge@gmail.com>
2024-07-10 20:07:00 +0300

    Set up basic HTML, CSS, and JavaScript for personal website

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+document.addEventListener("DOMContentLoaded", () => {
+  const h2 = document.createElement("h2");
+  h2.textContent = "This content added by JavaScript";
+  document.getElementById("js-content").appendChild(h2);
+
+  const button = document.getElementById("toggle-button");
+  button.addEventListener("click", () => {
+    h2.style.display = h2.style.display === "none" ? "block" : "none";
+  });
+});
